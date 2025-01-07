4aa1613fdff067d318e63bb48882251ee9fee868
unknown <elieseraraujo12@gmail.com>
2025-01-07 15:59:26 -0300

    Modificações básicas

--- calculadora-python/main.py
+++ calculadora-python/main.py
@@ -1,9 +1,26 @@
+import operator
+
 print("Olá, bem-vindo à calculadora!")
 
 a = int(input("Digite o primeiro número: "))
-b = int(input("Digite o segundo número: "))
+
+i = str(input("\nAgora selecione a operação desejada: "))
+
+b = int(input("\nDigite o Segundo número: "))
+
+if(i == '+'):
+    res = operator.add(a,b)
+elif (i == '-'):
+    res = operator.sub(a,b)
+elif (i == '*'):
+    res = operator.mul(a,b)
+elif (i == '/'):
+    res = operator.truediv(a,b)
+else:
+    print("Operação Inválida!")
+    exit()
 
 
 print("\nprocessando...")
-print(f'\nOs números digitados foram {a} e {b}.')
+print(f'\nO Resultado é {res}.')
 print("\nObrigado por utilizar!")
